2bdd85b67a7d235c8276166268bdd0f2cf5dcd03
Marek Polcar <marek@cilf.cz>
2017-05-06 18:02:31 +1200

    fix paths for static files and use production API

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const FETCH_ANIMAL = 'FETCH_ANIMAL';
 
-const ROOL_URL = 'http://localhost:8081/api/pedigree/animals/';
+const ROOL_URL = 'https://tapka.cz/api/pedigree/animals/';
 
 export function fetchAnimal(id) {
   const request = axios.get(`${ROOL_URL}${id}`);
