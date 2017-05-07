f128befe5885e7fb84b75203b6cd0752cb335e1a
Marek Polcar <marek@cilf.cz>
2017-05-07 14:19:04 +1200

    make clicking links rerender components

--- src/components/animal.js
+++ src/components/animal.js
@@ -5,23 +5,72 @@ import PropTypes from "prop-types";
 import AnimalCard from "./animal_card";
 import AnimalParent from "./animal";
 
+/**
+ * pujde se zbavit animalId? Idealne bych ve state chtel mit jenom animal
+ * pujde se zbavit active?
+ * pujde se zbavit znovunacitani jiz nactenych zvirat?
+ * pujde se zbavit loading pri dosazeni max depth?
+ */
+
 class Animal extends Component {
   static contextTypes = {
     router: PropTypes.object
   };
 
-  componentWillMount() {
+  constructor(props) {
+    super(props);
 
-    if (this.props.animalId) {
-      this.active = false;
-      this.animalId = this.props.animalId;
-    } else {
-      this.active = true;
-      this.animalId = this.props.match.params.id;
-    }
-    this.props.fetchAnimal(this.animalId, this.active);
+    this.state = {
+      active: false,
+      depth: null,
+      animalId: null
+    };
   }
 
+  componentWillMount() {
+
+    let state = {};
+    if (this.props.animalId) {
+      state = {
+        depth: this.props.depth + 1,
+        active: false,
+        animalId: this.props.animalId
+      };
+    } else {
+      state = {
+        depth: 0,
+        active: true,
+        animalId: this.props.match.params.id
+      };
+    }
+
+    if (state.depth > 2) {
+      return;
+    }
+
+    this.setState(state);
+
+    this.props.fetchAnimal(state.animalId, state.active);
+  }
+
+  componentWillReceiveProps(props) {
+    if (!props.match) {
+      return;
+    }
+
+    if (props.match.params.id === props.animals.active) {
+      return;
+    }
+
+    let state = {
+      depth: 0,
+      active: true,
+      animalId: props.match.params.id
+    };
+    this.setState(state);
+
+    this.props.fetchAnimal(state.animalId, state.active);
+  }
 
   renderParents(animal) {
 
@@ -29,7 +78,7 @@ class Animal extends Component {
     if (animal.dadId) {
       parents.push (
           <div className="col-sm-6" key={animal.dadId}>
-            <AnimalParent animalId={animal.dadId}/>
+            <AnimalParent animalId={animal.dadId} depth={this.state.depth}/>
           </div>
       );
     }
@@ -38,7 +87,7 @@ class Animal extends Component {
     if (animal.momId) {
       parents.push (
           <div className="col-sm-6" key={animal.momId}>
-            <AnimalParent animalId={animal.momId}/>
+            <AnimalParent animalId={animal.momId} depth={this.state.depth}/>
           </div>
        );
      }
@@ -48,7 +97,7 @@ class Animal extends Component {
 
   render() {
 
-    const animal = this.props.animals[this.active ? this.props.animals.active : this.animalId];
+    const animal = this.props.animals[this.state.active ? this.props.animals.active : this.state.animalId];
 
     if (!animal) {
       return <div>Loading...</div>
